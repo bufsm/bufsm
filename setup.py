5abf714ac1c016b1f416666ba02e9d0c45f7b719
Matheus Dal Mago <matheusdalmago10@hotmail.com>
2017-04-19 14:16:26 -0300

    Adding openshift Django project

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    # GETTING-STARTED: set your app name:
+    name='bUFSM',
+    # GETTING-STARTED: set your app version:
+    version='0.1',
+    # GETTING-STARTED: set your app description:
+    description='OpenShift App',
+    # GETTING-STARTED: set author name (your name):
+    author='Matheus Dal Mago',
+    # GETTING-STARTED: set author email (your email):
+    author_email='matheus.dalmago@ecomp.ufsm.br',
+    # GETTING-STARTED: set author url (your url):
+    url='http://www.python.org/sigs/distutils-sig/',
+    # GETTING-STARTED: define required django version:
+    install_requires=[
+        'Django==1.8'
+    ],
+    dependency_links=[
+        'https://pypi.python.org/simple/django/'
+    ],
+)

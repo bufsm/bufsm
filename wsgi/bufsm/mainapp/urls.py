a7e9c7bb85cf33ac32eaed0368bc0da3ccb2fb31
Matheus Dal Mago <matheusdalmago10@hotmail.com>
2017-04-21 11:32:50 -0300

    Simplifying URLs

--- wsgi/bufsm/mainapp/urls.py
+++ wsgi/bufsm/mainapp/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^linha/(?P<idLinha>[0-9]+)/get$', views.getLinha),
-    url(r'^linha/(?P<idLinha>[0-9]+)/write/(?P<token>.+)/(?P<lat>.+)/(?P<lng>.+)$', views.writeLinha),
+    url(r'^linha/(?P<idLinha>[0-9]+)$', views.getLinha),
+    url(r'^linha/(?P<idLinha>[0-9]+)/(?P<token>.+)/(?P<lat>.+)/(?P<lng>.+)$', views.writeLinha),
 ]

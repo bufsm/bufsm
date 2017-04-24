16ec662f8583c147b04a41417ff5e00e07213359
Iury Cleveston <iurycl@gmail.com>
2017-04-24 12:16:18 -0300

    Organize js, remove unused variables, improve performance, alter px to rem in css

--- Front-end/static/js/index.js
+++ Front-end/static/js/index.js
@@ -174,85 +174,17 @@ var pointsStop = [{
 ];
 
 var isMobile = false;
-var iconMarker;
-var iconBus;
-var busMarker;
-var urlAPI = "https://bufsm-dalmago.rhcloud.com/linha/1";
-
+var map;
+const urlAPI = "https://bufsm-dalmago.rhcloud.com/linha/1";
 var actualIndex = -1;
-
-//Iniatialize google maps API and go to the IoT portal to update List of things
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: center,
-        fullscreenControl: true,
-        gestureHandling: 'greedy',
-        scrollwheel: false,
-        zoom: 17,
-        streetViewControl: false
-    });
-
-    var busPath = new google.maps.Polyline({
-        path: pointsRoute,
-        geodesic: true,
-        strokeColor: '#0000FF',
-        strokeOpacity: 1.0,
-        strokeWeight: 2
-    });
-    iconBus = {
-        url: "static/img/bufsm2.png", // url
-        scaledSize: new google.maps.Size(25, 32), // scaled size
-        origin: new google.maps.Point(0, 0), // origin
-        anchor: new google.maps.Point(0, 0) // anchor
-    };
-    iconMarker = {
-        url: 'static/img/marker.png', // url
-        scaledSize: new google.maps.Size(30, 35), // scaled size
-        origin: new google.maps.Point(0, 0), // origin
-        anchor: new google.maps.Point(0, 0) // anchor
-    };
-    for (i = 0; i < pointsStop.length; i++) {
-        addBusStop(pointsStop[i]);
-    }
-    addBus(bufsmCurrentLocation);
-    busPath.setMap(map);
-    updatePosition();
-}
-
-function updatePosition() {
-    i = 0;
-    window.setInterval(function() {
-        var data;
-        $.get(urlAPI, function(data, status) {
-            bufsmCurrentLocation.lat = data.lat;
-            bufsmCurrentLocation.lng = data.lng;
-            updateDateSchedule(data.timeNow);
-        });
-        busMarker.setPosition(bufsmCurrentLocation);
-    }, 1000); // repeat forever, polling every 1 seconds
-}
-
-function addBusStop(location) {
-    marker = new google.maps.Marker({
-        position: location,
-        icon: iconMarker,
-        map: map
-    });
-}
-
-function addBus(location) {
-    busMarker = new google.maps.Marker({
-        position: location,
-        icon: iconBus,
-        map: map
-    });
-}
 
 $(document).ready(function() {
 
+    //Check if the device is mobile
     if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) ||
         /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4))) isMobile = true;
 
+    //Config the notification
     toastr.options.timeOut = 0;
     toastr.options.extendedTimeOut = 0;
     toastr.options.closeButton = true;
@@ -261,14 +193,18 @@ $(document).ready(function() {
         toastr.options.positionClass = 'toast-top-full-width';
     }
 
+    //Event for the bus-finding button
     $('#findBus').click(function(e) {
         e.preventDefault();
         map.setCenter(bufsmCurrentLocation);
     });
 
+    //Populate the modal with all the departure times
     $(leaveTime).each(function(i, e) {
         $('.modal-body ul').append('<li>' + e + '</li>');
     });
+
+    //Hide and show the menubar when clicked
     $('.cd-logo').click(function() {
         $("#hide").animate({
             width: 'toggle'
@@ -279,22 +215,154 @@ $(document).ready(function() {
         }, 3000);
     });
 
+    //Show the tooltip for the menubar and hide it after 3s
     $('#hide div[data-toggle="tooltip"]').tooltip('toggle');
     window.setTimeout(function() {
         $('#hide div[data-toggle="tooltip"]').tooltip('toggle');
     }, 3000);
 
+    //move nav element position according to window width
+    moveNavigation();
+    $(window).on('resize', function() {
+        (!window.requestAnimationFrame) ? setTimeout(moveNavigation, 300): window.requestAnimationFrame(moveNavigation);
+    });
+
+    //mobile version - open/close navigation
+    $('.cd-nav-trigger').on('click', function(event) {
+        event.preventDefault();
+        if ($('header').hasClass('nav-is-visible')) $('.moves-out').removeClass('moves-out');
+
+        $('header').toggleClass('nav-is-visible');
+        $('.cd-main-nav').toggleClass('nav-is-visible');
+        $('.cd-main-content').toggleClass('nav-is-visible');
+    });
+
+    //mobile version - go back to main navigation
+    $('.go-back').on('click', function(event) {
+        event.preventDefault();
+        $('.cd-main-nav').removeClass('moves-out');
+    });
+
+    //open sub-navigation
+    $('.cd-subnav-trigger').on('click', function(event) {
+        event.preventDefault();
+        $('.cd-main-nav').toggleClass('moves-out');
+    });
+
+    function moveNavigation() {
+        var navigation = $('.cd-main-nav-wrapper');
+        var screenSize = checkWindowWidth();
+        if (screenSize) {
+            //desktop screen - insert navigation inside header element
+            navigation.detach();
+            navigation.insertBefore('.cd-nav-trigger');
+        } else {
+            //mobile screen - insert navigation after .cd-main-content element
+            navigation.detach();
+            navigation.insertAfter('.cd-main-content');
+        }
+    }
+
+    function checkWindowWidth() {
+        var mq = window.getComputedStyle(document.querySelector('header'), '::before').getPropertyValue('content').replace(/"/g, '').replace(/'/g, "");
+        return (mq == 'mobile') ? false : true;
+    }
+
 });
 
-function updateDateSchedule(timeStamp) {
+//Iniatialize google maps API and go to the IoT portal to update List of things
+function initMap() {
 
+    //Create the map
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: center,
+        fullscreenControl: true,
+        gestureHandling: 'greedy',
+        scrollwheel: false,
+        zoom: 17,
+        streetViewControl: false
+    });
+
+    //Add the bus path to the map
+    new google.maps.Polyline({
+        path: pointsRoute,
+        geodesic: true,
+        strokeColor: '#0000FF',
+        strokeOpacity: 1.0,
+        strokeWeight: 2,
+        map: map
+    });
+
+    //For each stop
+    for (i = 0; i < pointsStop.length; i++) {
+
+        //Add a new stop to the map
+        new google.maps.Marker({
+            position: pointsStop[i],
+            icon: {
+                url: 'static/img/marker.png', // url
+                scaledSize: new google.maps.Size(30, 35), // scaled size
+                origin: new google.maps.Point(0, 0), // origin
+                anchor: new google.maps.Point(0, 0) // anchor
+            },
+            map: map
+        });
+    }
+
+    //Add the bus to the map
+    var busMarker = new google.maps.Marker({
+        position: bufsmCurrentLocation,
+        icon: {
+            url: "static/img/bufsm2.png", // url
+            scaledSize: new google.maps.Size(25, 32), // scaled size
+            origin: new google.maps.Point(0, 0), // origin
+            anchor: new google.maps.Point(0, 0) // anchor
+        },
+        map: map
+    });
+
+    //Update the bus position
+    updateBusPosition(busMarker);
+}
+
+function updateBusPosition(busMarker) {
+
+    //Every 1s
+    window.setInterval(function() {
+
+        //Perform the ajax get for new data
+        $.get(urlAPI, function(data, status) {
+            bufsmCurrentLocation.lat = data.lat;
+            bufsmCurrentLocation.lng = data.lng;
+
+            //Update the bus departure based on the server datetime
+            updateDeparture(data.timeNow);
+        });
+
+        //Update the bus position on the map
+        busMarker.setPosition(bufsmCurrentLocation);
+
+    }, 1000);
+}
+
+function updateDeparture(timeStamp) {
+
+    //Define the current hour, changing the text to a numeric value
     var currentHour = parseInt(timeStamp.slice(11, 13)) * 1000 + parseInt(timeStamp.slice(14, 16));
 
     var index = 0;
     var i = 0;
+
+    //For each departure time in the array
     for (i = 0; i < leaveTime.length - 1; i++) {
+
+        //Get the current departure
         var leaveHour = parseInt(leaveTime[i].slice(0, 2)) * 1000 + parseInt(leaveTime[i].slice(3, 5));
+
+        //Get the next departure
         var leaveHourNext = parseInt(leaveTime[i + 1].slice(0, 2)) * 1000 + parseInt(leaveTime[i + 1].slice(3, 5));
+
+        //When it finds, update the index to set new position in the array
         if (currentHour < leaveHourNext && currentHour >= leaveHour) {
             index = i + 1;
             break;
@@ -303,10 +371,17 @@ function updateDateSchedule(timeStamp) {
         }
     }
 
+    //If the index has changed
     if (index !== actualIndex) {
+
+        //Update the index
         actualIndex = index;
+
+        //Update the notification system
         toastr.clear()
         toastr.success('Próximo Ônibus sai às <strong>' + leaveTime[(index % leaveTime.length)] + '</strong>');
+
+        //Update the modal information, setting the new departure time
         $('.modal-body ul li').siblings().removeClass("busActive").find('span').remove();
         $('.modal-body ul li:nth-child(' + ((index % leaveTime.length) + 1) + ')').addClass("busActive").append('<span style="font-weight:bold;color:#880000"> - Próxima Saída </span>')
 

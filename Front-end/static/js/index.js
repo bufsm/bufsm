30e6d359a3e262a3df566c8be50550e9cf4ca06b
Iury Cleveston <iurycl@gmail.com>
2017-05-01 17:22:24 -0300

    User Geolocation Add Error Mesage

--- Front-end/static/js/index.js
+++ Front-end/static/js/index.js
@@ -367,19 +367,10 @@ function initMap() {
             user.setPosition(pos);
 
         }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
+            toastr.error('<strong>Habilite sua Localização (GPS)</strong>');
         });
     } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-    }
-
-    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-            'Error: The Geolocation service failed.' :
-            'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
+        toastr.error('<strong>Habilite sua Localização (GPS)</strong>');
     }
 
 }

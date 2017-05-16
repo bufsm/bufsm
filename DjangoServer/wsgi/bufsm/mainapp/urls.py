1935e7faa451eba549f75a8ef2814eb044a29c49
Matheus Dal Mago <matheusdalmago10@hotmail.com>
2017-05-16 19:57:13 -0300

    Removing server side

--- DjangoServer/wsgi/bufsm/mainapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    # url(r'^linha/(?P<idLinha>[0-9]+)$', views.testLinha),
-    url(r'^linha/(?P<idLinha>[0-9]+)$', views.getLinha),
-    url(r'^linha/(?P<idLinha>[0-9]+)/(?P<token>.+)/(?P<lat>.+)/(?P<lng>.+)$', views.writeLinha),
-]
--- Server/schema.sql
+++
@@ -1,7 +0,0 @@
-drop table if exists things;
-create table things (
-  thing_id integer primary key autoincrement,
-  thing_name text not null,
-  lat real,
-  lon real
-);

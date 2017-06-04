e8d0135fcad44b28752f18c07bedb63e6bd7dc42
Iury Cleveston <iurycl@gmail.com>
2017-06-04 15:28:52 -0300

    Code was Improved

--- front-end/static/js/index.js
+++ front-end/static/js/index.js
@@ -1,9 +1,10 @@
+"use strict";
+
 var leaveTime = ["07:30", "08:00", "08:30", "09:00",
   "09:30", "10:00", "11:00", "11:30", "12:30", "13:00",
   "13:30", "14:00", "15:00", "16:00", "16:30",
   "17:00", "17:30", "18:30", "19:30", "20:30", "21:30", "22:00"
 ];
-
 var bufsmCurrentLocation = {
   lat: null,
   lng: null
@@ -12,7 +13,12 @@ var userCurrentLocation = {
   lat: null,
   lng: null
 };
-var i;
+
+var defaultMapCenter = {
+  lat: -29.710173,
+  lng: -53.716594
+};
+
 var pointsRoute = [{
     lat: -29.710173,
     lng: -53.716594
@@ -178,101 +184,137 @@ var map;
 var user;
 var busMarker;
 var actualIndex = -1;
-var error = false;
-var firstTime = true;
+var isFirstMessage = false;
+var isConnected = false;
+var mqttClient = new Paho.MQTT.Client('iot.eclipse.org', 443, "/ws", '');
+var ip = null;
+var prevPos = {
+  lat: 0,
+  lng: 0
+};
 
-//Set the MQTT client
-var client = new Paho.MQTT.Client('iot.eclipse.org', 443, "/ws", '');
+//Check if the device is mobile
+if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) ||
+  /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4))) isMobile = true;
 
-client.connect({
+//Config the notification
+toastr.options.timeOut = 0;
+toastr.options.extendedTimeOut = 0;
+toastr.options.closeButton = true;
+
+if (isMobile) {
+  toastr.options.positionClass = 'toast-top-full-width';
+}
+
+//Define the connection options
+var connectionOptions = {
   useSSL: true,
-  keepAliveInterval: 15,
+  keepAliveInterval: 20,
   onSuccess: function() {
-    client.subscribe("b123");
-
-    //If the user is admin
-    var admin = location.search.split('admin=')[1]
+    mqttClient.subscribe("bufsm/p");
+    mqttClient.subscribe("bufsm/m");
+    isConnected = true;
+    isFirstMessage = false;
 
     // Try HTML5 geolocation.
     if (navigator.geolocation) {
+
+      //Watch the position
       navigator.geolocation.watchPosition(function(position) {
-        var pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
 
-        user.setPosition(pos);
+          //Get the user position
+          var pos = {
+            lat: Math.round(position.coords.latitude * 1000000) / 1000000,
+            lng: Math.round(position.coords.longitude * 1000000) / 1000000
+          };
 
-        //Send the new position to the broker
-        if (admin == 'true') {
-          message = new Paho.MQTT.Message(String(pos.lat).slice(4, 10) + ',' + String(pos.lng).slice(4, 10));
-          message.destinationName = "b123";
-          message.retained = true;
-          client.send(message);
+          //Set the user icon position on the map
+          user.setPosition(pos);
 
-        }
+          var info = {
+            'ip': ip,
+            'position': {
+              'lat': pos.lat,
+              'lon': pos.lng
+            }
+          };
 
-      }, function() {
-        error = true;
-        toastr.error('<strong>Habilite sua Localização (GPS)</strong>');
-      });
-    } else {
-      error = true;
-      toastr.error('<strong>Habilite sua Localização (GPS)</strong>');
+          var shouldSend = (Math.abs(pos.lat) - Math.abs(prevPos.lat)) > 0.0001 || (Math.abs(pos.lon) - Math.abs(prevPos.lon)) > 0.0001;
+
+          //Update the prev position
+          prevPos = pos;
+
+          if (ip && isConnected && shouldSend) {
+            var message = new Paho.MQTT.Message(JSON.stringify(info));
+            message.destinationName = "bufsm/o";
+            message.retained = false;
+            mqttClient.send(message);
+          }
+
+        },
+        function() {
+          toastr.error('<strong>Habilite sua Localização (GPS)</strong>');
+        });
     }
 
   }
-});
+};
 
 //When the connection is lost
-client.onConnectionLost = function onConnectionLost(responseObject) {
+mqttClient.onConnectionLost = function onConnectionLost(responseObject) {
   if (responseObject.errorCode !== 0) {
     toastr.remove();
     toastr.error('<strong>Verifique sua Conexão!</strong>');
-    firstTime = true;
+
+    isConnected = false;
+
+    mqttClient.connect(connectionOptions);
+
   }
 };
 
 //When the message is received
-client.onMessageArrived = function onMessageArrived(message) {
+mqttClient.onMessageArrived = function onMessageArrived(message) {
 
-  if (message.payloadString == 'e') {
+  if (message.destinationName === 'bufsm/m' && message.payloadString === 'e') {
     toastr.remove();
     toastr.warning('<strong>Aguarde. Estamos localizando o bUFSM!</strong>');
-    firstTime = true;
-  } else {
+
+    isFirstMessage = false;
+
+  } else if (message.destinationName === 'bufsm/p') {
 
     //Parse JSON
-    data = message.payloadString.split(',')
+    var lat = message.payloadString.slice(0, 6);
+    var lon = message.payloadString.slice(6, 12);
 
     //Get the position
-    bufsmCurrentLocation.lat = -29 - parseFloat('0.' + data[0]);
-    bufsmCurrentLocation.lng = -53 - parseFloat('0.' + data[1]);
+    bufsmCurrentLocation.lat = -29 - parseFloat('0.' + lat);
+    bufsmCurrentLocation.lng = -53 - parseFloat('0.' + lon);
 
-    if (firstTime) {
-      map.setCenter(bufsmCurrentLocation);
-      firstTime = false;
-    }
+    //Update the bus position
     busMarker.setPosition(bufsmCurrentLocation);
 
+    if (!isFirstMessage) {
+
+      toastr.remove();
+
+      //Update the bus departure
+      updateDeparture(new Date().toLocaleString('pt-BR'));
+
+      //Center the map in the bus position
+      map.setCenter(bufsmCurrentLocation);
+
+      //Display the link to find my bus
+      document.getElementById('findBus').style.display = 'block';
+
+      isFirstMessage = true;
+    }
   }
 
 };
 
 $(document).ready(function() {
-
-  //Check if the device is mobile
-  if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) ||
-    /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4))) isMobile = true;
-
-  //Config the notification
-  toastr.options.timeOut = 0;
-  toastr.options.extendedTimeOut = 0;
-  toastr.options.closeButton = true;
-
-  if (isMobile) {
-    toastr.options.positionClass = 'toast-top-full-width';
-  }
 
   //Event for the bus-finding button
   $('#findBus').click(function(e) {
@@ -311,8 +353,10 @@ $(document).ready(function() {
 
   });
 
-  //Update the bus departure
-  updateDeparture(new Date().toLocaleString('pt-BR'));
+  //Find the external ip
+  $.get('http://jsonip.com/', function(r) {
+    ip = r.ip
+  });
 
   //Update the bus departure based on the server datetime
   window.setInterval(function() {
@@ -377,7 +421,7 @@ $(document).ready(function() {
 
 });
 
-//Iniatialize google maps API and go to the IoT portal to update List of things
+//Iniatialize google maps API
 function initMap() {
 
   //Create the map
@@ -393,9 +437,7 @@ function initMap() {
   busMarker = new google.maps.Marker({
     icon: {
       url: "static/img/bufsm2.png", // url
-      scaledSize: new google.maps.Size(25, 32), // scaled size
-      origin: new google.maps.Point(0, 0), // origin
-      anchor: new google.maps.Point(0, 0) // anchor
+      scaledSize: new google.maps.Size(25, 32)
     },
     map: map
   });
@@ -410,21 +452,18 @@ function initMap() {
     map: map
   });
 
-  //For each stop
-  for (i = 0; i < pointsStop.length; i++) {
+  //Add a new stop to the map
+  pointsStop.forEach(function(e) {
 
-    //Add a new stop to the map
     new google.maps.Marker({
-      position: pointsStop[i],
+      position: e,
       icon: {
         url: 'static/img/marker.png', // url
-        scaledSize: new google.maps.Size(30, 35), // scaled size
-        origin: new google.maps.Point(0, 0), // origin
-        anchor: new google.maps.Point(0, 0) // anchor
+        scaledSize: new google.maps.Size(30, 30), // scaled size
       },
       map: map
     });
-  }
+  });
 
   //Add the user to the map
   user = new google.maps.Marker({
@@ -434,6 +473,12 @@ function initMap() {
     },
     map: map
   });
+
+  //Set the default map center
+  map.setCenter(defaultMapCenter);
+
+  //Connect the MQTT client
+  mqttClient.connect(connectionOptions);
 
 }
 
@@ -469,11 +514,6 @@ function updateDeparture(timeStamp) {
     //Update the index
     actualIndex = index;
 
-    //Update the notification system
-    if (!error) {
-      toastr.clear();
-      error = false;
-    }
     toastr.success('Próximo Ônibus sai às <strong>' + leaveTime[(index % leaveTime.length)] + '</strong>');
 
     //Update the modal information, setting the new departure time
